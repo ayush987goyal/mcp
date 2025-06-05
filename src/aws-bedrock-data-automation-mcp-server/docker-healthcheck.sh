78f2bac422f0c1879b3370d15bd11f7ba33ed0c1
Ayush Goyal <goyalayu@amazon.com>
2025-06-05 08:18:54 +0530

    fix shebang

--- src/aws-bedrock-data-automation-mcp-server/docker-healthcheck.sh
+++ src/aws-bedrock-data-automation-mcp-server/docker-healthcheck.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/sh
 
 if [ "$(lsof +c 0 -p 1 | grep -e "^awslabs\..*\s1\s.*\sunix\s.*socket$" | wc -l)" -ne "0" ]; then
   echo -n "$(lsof +c 0 -p 1 | grep -e "^awslabs\..*\s1\s.*\sunix\s.*socket$" | wc -l) awslabs.* streams found";
